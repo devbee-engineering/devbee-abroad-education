659a172f571b3167b0a8c837de80386af81dbe84
Muba <mubasir@dev-bee.com>
2025-08-16 16:37:14 +0530

    feat: Simplify logo paths in Home component for improved asset loading

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import {  HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>,
